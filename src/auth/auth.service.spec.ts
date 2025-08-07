fa9838fc7909dab62a2b12107a6665ff6f5c59c7
Sundas <sundaskhanhere@gmail.com>
2025-08-07 23:43:25 +0500

    removed commented ocde

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,23 +1,3 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AuthService } from './auth.service';
-
-// describe('AuthService', () => {
-//   let service: AuthService;
-
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [AuthService],
-//     }).compile();
-
-//     service = module.get<AuthService>(AuthService);
-//   });
-
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
-
-// auth.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from 'src/prisma.service';
 import { AuthService } from './auth.service';
